c0753b19f6bc61fd04c539d1f166133e2bb5bc0e
Alva <alvalina@live.se>
2025-11-26 14:47:38 +0100

    Börjad implementation av funktioner

--- include/navigation.h
+++ include/navigation.h
@@ -4,21 +4,39 @@
 class Navigation{
 
     private:
-        static float coordinates[];
+
+        static const int MAX_ROUTE_LEN = 16;      // max antal stopp i en rutt
+        static const int MAX_QR_ID_LEN = 16;      // max längd på ett QR-id typ "monaLisa"
+        int  routeLength;      
+        int  currentTargetIndex;  
+
+        char routeIDs[MAX_ROUTE_LEN][MAX_QR_ID_LEN];
+        float coordinates[2];
+        float headingRad;
+        
+        
         void initGyroCalibration();
-        void calibrateFromQR();
+        void calibrateFromQR(const char* qrId);
         void readGyro();
         void readEncoders();
         void readAccelerometer(); 
         
-        // kanske ha en getConfidanceValue(); ??
+
+        // void fusionPos(?, time interval)  // sensor fusion mellan gyro + enkodrar 
+        // kanske ha en getConfidenceValue(); // updateConfidence ??
+
+        // void setRout()
+        
 
 
 
 
     public:
     Navigation();
-    void getCoordinates(float coords[]); // förväntar oss en array med 2 floats (x & y kordinater )
+    void getCoordinates(float coords[2]) const; // förväntar oss en array med 2 floats (x & y kordinater )
+    float getHeadingDeg() const;  // för logg/UI
+
+    void updateRoute(const char* route[], int length);
 
 
 
--- src/navigation.cpp
+++ src/navigation.cpp
@@ -1,12 +1,42 @@
 #include "navigation.h"
+#include <string.h>   
+#include <math.h>  
 
-Navigation::Navigation(){
-    float coordinates[2] = {0,0};
+Navigation::Navigation(): 
+    routeLength(0),
+    currentTargetIndex(0),
+    headingRad(0.0f)
+    {
+        coordinates[0] = 0.0f;
+        coordinates[1] = 0.0f;
+
+
+        for (int i = 0; i < MAX_ROUTE_LEN; ++i) {
+        routeIDs[i][0] = '\0';
+    }
+
+    initGyroCalibration();
 }
 
 void Navigation::initGyroCalibration(){}
 
-void Navigation::calibrateFromQR(){}
+void Navigation::calibrateFromQR(const char* qrId){ //strukturera om så att korrigering sker även om oväntat QR avläses
+    
+    if (qrId == nullptr) {  // null case
+        return;
+    }
+    
+    if (strcmp(qrId, routeIDs[currentTargetIndex]) == 0) {
+       
+        currentTargetIndex++; //rätt QR-kod hittad, vidare till nästa
+
+        // TODO: Själva korrigeringen
+        
+    } else {
+        // oväntat QR avläst, kanske loggas? 
+    } 
+
+}
 
 void Navigation::readGyro(){}
 
@@ -14,6 +44,30 @@ void Navigation::readEncoders(){}
 
 void Navigation::readAccelerometer(){}
 
-void Navigation::getCoordinates(float coords[]){}
+void Navigation::getCoordinates(float coords[2]) const{
+        coords[0] = coordinates[0];
+        coords[1] = coordinates[1];
+}
+
+float Navigation::getHeadingDeg() const {
+    return headingRad * 180.0f / static_cast<float>(M_PI);
+}
+
+void Navigation::updateRoute(const char* route[], int length) {
+    routeLength = 0;
+    currentTargetIndex = 0;
+
+    if (!route || length <= 0) {
+        return;
+    }
+
+    int maxLen = (length < MAX_ROUTE_LEN) ? length : MAX_ROUTE_LEN;
+
+    for (int i = 0; i < maxLen; ++i) {
+        strncpy(routeIDs[i], route[i], MAX_QR_ID_LEN - 1);
+        routeIDs[i][MAX_QR_ID_LEN - 1] = '\0';
+        routeLength++;
+    }
+}
 
 
