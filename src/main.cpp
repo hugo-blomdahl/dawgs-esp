1434b9958b1ee87c1d21bd461ce3432daea60297
Hugo Blomdahl <blomdahl.hugo@protonmail.com>
2025-12-08 13:00:00 +0100

    update server ip

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,7 @@ extern "C" void app_main(void) {
 
     Comms comms;
 
-    TcpClient* tcpClient = new TcpClient("ASUS_20_slow", "Steadily8638", "192.168.0.199", 8089);
+    TcpClient* tcpClient = new TcpClient("Hugo telefooonnn", "hugowifi12333", "10.245.112.237", 8089);
     tcpClient->start();
 
     ESP_LOGI(TAG, "All components started");
